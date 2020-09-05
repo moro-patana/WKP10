9c9a02a839355402c07e1755c7c51307f4787d99
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-09-05 17:18:45 +0300

    modified the pupup in edit partner

--- script.js
+++ script.js
@@ -40,10 +40,10 @@ const displayList = data => {
         <td>${person.jobArea}</td>
         <td>${person.phone}</td>
         <td>
-            <button class="edit" data-id="${person.id}">
+            <button class="edit" value="${person.id}">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="pencil w-6 h-6"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
             </button>
-            <button class="delete" data-id="${person.id}">
+            <button class="delete" value="${person.id}">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="trash w-6 h-6"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
             </button>
             </td>
@@ -56,13 +56,16 @@ const editPartner = (e) => {
 	const editBtn = e.target.closest('button.edit');
 	if (editBtn) {
 		e.preventDefault();
-		const id = e.target.dataset.value;
+		const id = e.target.value;
+		console.log(id);
 		editPartnerPopup(id);
 	}
 }
 // Create an form html to edit the parteners profile
-const editPartnerPopup = (e) => {
+const editPartnerPopup = (id) => {
 	return new Promise(async function(resolve, reject) {
+		const person = persons.find(person => person.id === id );
+		console.log(id);
 		const popup = document.createElement(`form`);
 		popup.classList.add(`popup`);
 		popup.innerHTML = `
@@ -74,7 +77,8 @@ const editPartnerPopup = (e) => {
 			required
 			name="lastname"
 			id="lastname"
-			placeholder="${faker.name.lastName()}"
+			value
+			="${person.lastName}"
 		/>
 	</fieldset>
 	<fieldset>
@@ -84,7 +88,8 @@ const editPartnerPopup = (e) => {
 			required
 			name="firstName"
 			id="firstName"
-			placeholder="${faker.name.firstName()}"
+			value
+			="${person.firstName}"
 		/>
 	</fieldset>
 	<fieldset>
@@ -94,7 +99,8 @@ const editPartnerPopup = (e) => {
 			required
 			name="jobTitle"
 			id="jobTitle"
-			placeholder="${faker.name.jobTitle()}"
+			value
+			="${person.jobTitle}"
 		/>
 	</fieldset>
 	<fieldset>
@@ -104,7 +110,8 @@ const editPartnerPopup = (e) => {
 			required
 			name="jobArea"
 			id="jobArea"
-			placeholder="${faker.name.jobArea()}"
+			value
+			="${person.jobArea}"
 		/>
 	</fieldset>
 	<fieldset>
@@ -114,7 +121,8 @@ const editPartnerPopup = (e) => {
 			required
 			name="phone"
 			id="phone"
-			placeholder="${faker.phone.phoneNumber()}"
+			value
+			="${person.phone}"
 		/>
 	</fieldset>
 	<button class="save">Save</button>`;
@@ -135,8 +143,16 @@ const editPartnerPopup = (e) => {
 // Listen to the submit button to save the changes
 	popup.addEventListener('submit', (e) => {
 		e.preventDefault();
-		resolve(e.target.input.value);
-		destroyPopup(popup);
+		// resolve(e.target.input.value);
+		const newPerson = {};
+		const form = e.currentTarget;
+		id: id;
+		lastName: form.value;
+		firstName: form.value;
+		jobTitle: form.value;
+		jobArea: form.value;
+		phone: form.value;
+		console.log(newPerson);
 	},
 	{ once: true}
 	)
