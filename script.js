d7dce9419a2551007f6ca9e44511342fba6df183
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-09-05 16:11:49 +0300

    created popup and listen to the buttons

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 import faker from 'faker';
+function wait(ms = 0) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 const tbody = document.querySelector('tbody');
-
+const outerModal = document.querySelector('.outer-modal');
+const innerModal = document.querySelector('.inner-modal');
+async function destroyPopup(popup) {
+	popup.classList.remove('open');
+	await wait(1000);
+	popup.remove();
+	popup = null;
+}
 let persons = Array.from({ length: 10 }, () => {
 	return {
 		id: faker.random.uuid(),
@@ -26,33 +37,138 @@ const displayList = data => {
         <td>${person.jobArea}</td>
         <td>${person.phone}</td>
         <td>
-            <button class="edit">
+            <button class="edit" data-id="${person.id}">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="pencil w-6 h-6"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
             </button>
-            <button class="delete">
+            <button class="delete" data-id="${person.id}">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="trash w-6 h-6"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
             </button>
             </td>
     </tr>
-`
-		)
+`)
 		.join('');
 };
-
-const editPartner = () => {
-	// code edit function here
+const editPartner = (e) => {
+	const editBtn = e.target.closest('button.edit');
+	if (editBtn) {
+		e.preventDefault();
+		const id = e.target.dataset.value;
+		editPartnerPopup(id);
+	}
+}
+const editPartnerPopup = (e) => {
+	return new Promise(async function(resolve, reject) {
+		const popup = document.createElement(`form`);
+		popup.classList.add(`popup`);
+		popup.innerHTML = `
+		<h4>Edit Partener</h4>
+	<fieldset>
+		<label for="lastname">Lastname</label>
+		<input
+			type="text"
+			required
+			name="lastname"
+			id="lastname"
+			placeholder="${faker.name.lastName()}"
+		/>
+	</fieldset>
+	<fieldset>
+		<label for="firstName">Firstname</label>
+		<input
+			type="text"
+			required
+			name="firstName"
+			id="firstName"
+			placeholder="${faker.name.firstName()}"
+		/>
+	</fieldset>
+	<fieldset>
+		<label for="jobTitle">Job title</label>
+		<input
+			type="text"
+			required
+			name="jobTitle"
+			id="jobTitle"
+			placeholder="${faker.name.jobTitle()}"
+		/>
+	</fieldset>
+	<fieldset>
+		<label for="jobArea">Job Area</label>
+		<input
+			type="text"
+			required
+			name="jobArea"
+			id="jobArea"
+			placeholder="${faker.name.jobArea()}"
+		/>
+	</fieldset>
+	<fieldset>
+		<label for="phone">Phone Number</label>
+		<input
+			type="text"
+			required
+			name="phone"
+			id="phone"
+			placeholder="${faker.phone.phoneNumber()}"
+		/>
+	</fieldset>
+	<button class="save">Save</button>`;
+	if (reject) {
+		const skipBtn = document.createElement('button');
+		skipBtn.type = 'button';
+		skipBtn.textContent = "Cancel";
+		skipBtn.classList.add("cancel");
+		popup.appendChild(skipBtn);
+		skipBtn.addEventListener('click', () => {
+			resolve(null);
+			destroyPopup(popup);
+},
+	{ once: true}
+	)
+	}
+	popup.addEventListener('submit', (e) => {
+		e.preventDefault();
+		resolve(e.target.input.value);
+		destroyPopup(popup);
+	},
+	{ once: true}
+	)
+		document.body.appendChild(popup);
+		await wait(50);
+		popup.classList.add('open');
+	})
+}
+const deletePartner = (e) => {
+	const deleteBtn = e.target.closest('button.delete');
+	if (deleteBtn) {
+		e.preventDefault();
+		const id = e.target.dataset.value;
+		deletePartnerPopup(id);
+	}
 };
 
-const editPartnerPopup = () => {
-	// create edit popup here
-};
-
-const deletePartner = () => {
-	// code delete function gere
-};
-
-const deleteDeletePopup = () => {
+const deletePartnerPopup = () => {
 	// create confirmation popup here
-};
+	return new Promise(async function(resolve) {
+		const modal = document.createElement('div');
+		modal.classList.add('modal');
+		modal.innerHTML = `
+			<p class="confirm">Doyou want to delete?</p>
+			<div class="button">
+			<button class="yes">Yes</buton>
+			<button class="no">No</buton>
+			</div>
+		`;
+		document.body.appendChild(modal);
+		await wait(50);
+		modal.classList.add('open');
+
+	})
+}
+
+
+tbody.addEventListener('click', editPartner);
+tbody.addEventListener('click', deletePartner);
+ 
 
 displayList(persons);
